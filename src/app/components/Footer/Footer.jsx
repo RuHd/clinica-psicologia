add6a58db049ab0403d2ab5289d2c4104da234aa
RuHd <ruaan.ram@gmail.com>
2025-03-21 14:13:30 -0300

    Footer.jsx style update

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import './Footer.scss'
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaWhatsappSquare } from "react-icons/fa";
+
 
 const Footer = () => {
   return (
     <footer>
         <h2>Mídias Sociais</h2>
         <nav>
-            <button>Facebook</button>
-            <button>Instagram</button>
-            <button>Linkedin</button>
-            <button>Whatsapp</button>
+            <button>
+              <FaFacebook size={40}/>
+            </button>
+            <button>
+              <FaInstagramSquare size={40}/>
+            </button>
+            <button>
+              <FaLinkedin size={40}/>
+            </button>
+            <button>
+              <FaWhatsappSquare size={40}/>
+            </button>
         </nav>
         <p>Site feito por Ruan Mesquita - 2025</p>
     </footer>
