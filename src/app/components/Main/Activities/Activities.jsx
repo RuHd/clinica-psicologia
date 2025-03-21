26008d4a36d0503171efe5cf830f2497e1666887
RuHd <ruaan.ram@gmail.com>
2025-03-21 14:04:28 -0300

    Activities.jsx style addon

--- src/app/components/Main/Activities/Activities.jsx
+++ src/app/components/Main/Activities/Activities.jsx
@@ -1,21 +1,38 @@
 import React from 'react'
-
-const ServiceCard = () => {
-    return (
-        <article>
-            <h3>Serviços</h3>
-            <section>IMG</section>
-            <p>Descrição do serviço</p>
-        </article>
-    )
-}
+import { RiPsychotherapyFill } from "react-icons/ri";
+import { TbRibbonHealth } from "react-icons/tb";
+import { GiHealthNormal } from "react-icons/gi";
 
 const Activities = () => {
   return (
-    <ul>
-        <ServiceCard/>
-        <ServiceCard/>
-        <ServiceCard/>
+    <ul className='activities-list'>
+       <article>
+            <section>
+              <TbRibbonHealth size={70}/>
+              <h3>Cuidado pessoal</h3>
+            </section>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non provident nostrum assumenda ipsa a voluptate dolore sequi id quidem quos. Voluptates illum nesciunt facilis amet delectus repellendus dicta possimus cupiditate.
+            </p>
+        </article>
+        <article>
+            <section>
+              <GiHealthNormal size={70}/>
+              <h3>Saúde Mental</h3>
+            </section>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non provident nostrum assumenda ipsa a voluptate dolore sequi id quidem quos. Voluptates illum nesciunt facilis amet delectus repellendus dicta possimus cupiditate.
+            </p>
+        </article>
+        <article>
+            <section>
+              <RiPsychotherapyFill size={70}/>
+              <h3>Atendimento Humanizado</h3>
+            </section>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non provident nostrum assumenda ipsa a voluptate dolore sequi id quidem quos. Voluptates illum nesciunt facilis amet delectus repellendus dicta possimus cupiditate.
+            </p>
+        </article>
     </ul>
   )
 }
